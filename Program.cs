e14317cc9db9742799015af75ec9c4aec15aa27b
GustavoAguas <67701130+GustavoAguas@users.noreply.github.com>
2023-12-13 12:53:01 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/* Universidad de las Fuerzas Armadas ESPE
+ * Nombre: Gustavo Aguas
+ * Fecha: 13/12/2023
+ * Nrc:14956
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ejercicio 1
+        Console.WriteLine("Ejercicio 1 \n");
+        EvaluacionParcial1.Ejercicios.Ejercicio1.EjecutarEjercicio1();
+        Console.WriteLine("--------------------------------------------------");
+
+        // Ejercicio 2
+        Console.WriteLine("\nEjercicio 2 \n");
+        EvaluacionParcial1.Ejercicios.Ejercicio2.EjecutarEjercicio2();
+        Console.WriteLine("--------------------------------------------------");
+
+
+        //Ejercicio 3
+        Console.WriteLine("\nEjercicio 3 \n");
+        EvaluacionParcial1.Ejercicios.Ejercicio3.EjecutarEjercicio3();
+        Console.WriteLine("--------------------------------------------------");
+
+
+        // Ejercicio 4
+        Console.WriteLine("\nEjercicio 4 \n");
+        EvaluacionParcial1.Ejercicios.Ejercicio4.EjecutarEjercicio4();
+        Console.WriteLine("--------------------------------------------------");
+
+        // Ejercicio 5
+        Console.WriteLine("\nEjercicio 5");
+        EvaluacionParcial1.Ejercicios.Ejercicio5.EjecutarEjercicio5();
+        Console.WriteLine("--------------------------------------------------");
+    }
+}
